b2808e2947d77341bcec04c175882bd10db3d782
jiyuujin <jiyuujinunite@gmail.com>
2022-11-05 21:34:38 +0900

    update home name

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Atomic Design',
+      title: 'FlutterKaigi 2022',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Atomic Design Demo Home Page'),
+      home: const MyHomePage(title: 'FlutterKaigi 2022 Demo Home Page'),
     );
   }
 }
